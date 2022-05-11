b40ec6d13eaeeb7a006e1303b75b4867f91babb3
konstantin <kostuxa@icloud.com>
2022-05-11 20:59:29 +0300

    github actions

--- redirect_django/redirect/urls.py
+++ redirect_django/redirect/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 from links.views import RedirectView
 
